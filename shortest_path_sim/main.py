7a2c7541ace792f593fdd5adad594ffac4cb2b0f
KiRtAp2 <user@DESKTOP-GFIHPUD.localdomain>
2017-11-09 18:44:35 +0100

    Malo počistil kodo

--- shortest_path_sim/main.py
+++ shortest_path_sim/main.py
@@ -1,119 +1,14 @@
 import math
 import pygame
 import graph
-from algoritmi.bfs.bfs_ss_draw import bfs_step, bfs_done
+from algoritmi.bfs.bfs_ss_draw import bfs_step
 from shortest_path_sim import constants, colors
 from sys import exit as quit
 import algoritmi.bfs.bfs_ss_draw as bfs_
 
 
-window = pygame.display.set_mode((constants.window_width, constants.window_height))
-
-
-seznam_sosednjosti = []
-for i in range(constants.all_squares):
-    """ konstruira posamezne sosednje elemente """
-
-    l = []
-
-    if i >= constants.number_of_squares[0]: l.append(i-constants.number_of_squares[0])
-    # če vozlišče ni na zgornjem robu, dodaj v sosednje elemente vozlišče nad i
-
-    if i % constants.number_of_squares[0] != 0: l.append(i-1)
-    # če vozlišče ni na levem robu, dodaj v sosednje elemente vozlišče levo od i
-
-    if i < (constants.all_squares-constants.number_of_squares[0]): l.append(i+constants.number_of_squares[0])
-    # če vozlišče ni v spodnji vrsti, dodaj v sosednjost tistega pod i
-
-    if ((i+1) % constants.number_of_squares[0]) != 0: l.append(i+1)
-    # če vozlišče ni na desnem robu, dodaj v sosednjost tistega desno od i
-
-    seznam_sosednjosti.append(l)
-    print(i, l)
-
-
-graf = graph.GrafSS(constants.all_squares, seznam_sosednjosti)
-in_progress = ""
-graf.vozlica[0].oznaka = 3
-graf.vozlica[-1].oznaka = 3
-
-
-def narisi_kvadrate():
-    for vx in range(constants.number_of_squares[0]):
-        for vy in range(constants.number_of_squares[1]):
-            if (graf.vozlica[ vy*constants.number_of_squares[1]+vx ].oznaka is None) or (graf.vozlica[ vy*constants.number_of_squares[1]+vx ].oznaka == 0):
-                pygame.draw.rect(
-                    window,
-                    colors.kvadrat,
-                    (
-                        constants.border_width + (vx) * constants.square_width,  # x
-                        constants.border_height + (vy) * constants.square_height,  # y
-                        constants.square_width,  # sx
-                        constants.square_height  # sy
-                    )
-                )
-            elif graf.vozlica[ vy*constants.number_of_squares[1]+vx ].oznaka == 1:
-                pygame.draw.rect(
-                    window,
-                    colors.pobarvan,
-                    (
-                        constants.border_width + (vx) * constants.square_width,  # x
-                        constants.border_height + (vy) * constants.square_height,  # y
-                        constants.square_width,  # sx
-                        constants.square_height  # sy
-                    )
-                )
-            elif graf.vozlica[ vy*constants.number_of_squares[1]+vx ].oznaka == 2:
-                pygame.draw.rect(
-                    window,
-                    colors.obiskan,
-                    (
-                        constants.border_width + (vx) * constants.square_width,  # x
-                        constants.border_height + (vy) * constants.square_height,  # y
-                        constants.square_width,  # sx
-                        constants.square_height  # sy
-                    )
-                )
-
-            elif graf.vozlica[ vy*constants.number_of_squares[1]+vx ].oznaka == 3:
-                pygame.draw.rect(
-                    window,
-                    colors.start_stop,
-                    (
-                        constants.border_width + (vx) * constants.square_width,  # x
-                        constants.border_height + (vy) * constants.square_height,  # y
-                        constants.square_width,  # sx
-                        constants.square_height  # sy
-                    )
-                )
-
-
-def clear_board():
-    global in_progress
-    for v in graf.vozlica:
-        v.obiskano = False
-        if v.oznaka == 2: v.oznaka = 0
-        v.cas_vhoda = float('infinity')
-        v.cas_izhoda = float('infinity')
-    in_progress = ""
-    bfs_.reset()
-
-
-
-
-def kvadrat_na_xy(x, y):
-    if x >= constants.border_width+constants.square_field_width: return None
-    if y >= constants.border_height+constants.square_field_height: return None
-    if x < constants.border_width: return None
-    if y < constants.border_height: return None
-    x -= constants.border_width
-    y -= constants.border_height
-    sqx = math.floor(x / constants.square_width)
-    sqy = math.floor(y / constants.square_height)
-    return sqx+sqy*constants.number_of_squares[0]
-
-
-def dobi_sosede(k):
+def dobi_sosede(k: int):
+    """Vrne vse celice, ki so zraven k. To niso celice, ki jih lahko obiščemo iz k!"""
     # k: kvadrat
 
     l = []
@@ -132,6 +27,72 @@ def dobi_sosede(k):
 
     return l
 
+
+# ustvari seznam sosednjosti
+seznam_sosednjosti = []
+for i in range(constants.all_squares):
+    """ konstruira posamezne sosednje elemente """
+    l = dobi_sosede(i)
+    seznam_sosednjosti.append(l)
+
+
+# ustvari graf, okno ipd.
+window = pygame.display.set_mode((constants.window_width, constants.window_height))
+graf = graph.GrafSS(constants.all_squares, seznam_sosednjosti)
+in_progress = ""
+graf.vozlica[0].oznaka = 3
+graf.vozlica[-1].oznaka = 3
+
+
+def narisi_kvadrate():
+    """Funkcija pobarva del zaslona glede na oznako posameznega vozlišča"""
+    for vx in range(constants.number_of_squares[0]):
+        for vy in range(constants.number_of_squares[1]):
+            barva = (0, 0, 0)
+            oznaka = graf.vozlica[ vy*constants.number_of_squares[1]+vx ].oznaka
+            if (oznaka is None) or (oznaka == 0):
+                barva = colors.kvadrat
+            elif oznaka == 1:
+                barva = colors.pobarvan
+            elif oznaka == 2:
+                barva = colors.obiskan
+            elif oznaka == 3:
+                barva = colors.start_stop
+            pygame.draw.rect(
+                window,
+                barva,
+                (
+                    constants.border_width + vx * constants.square_width,  # x
+                    constants.border_height + vy * constants.square_height,  # y
+                    constants.square_width,  # sx
+                    constants.square_height  # sy
+                )
+            )
+
+
+def clear_board():
+    """Počisti polje"""
+    global in_progress
+    for v in graf.vozlica:
+        v.obiskano = False
+        if v.oznaka == 2: v.oznaka = 0
+        v.cas_vhoda = float('infinity')
+        v.cas_izhoda = float('infinity')
+    in_progress = ""
+    bfs_.reset()
+
+
+def kvadrat_na_xy(x, y):
+    """Vrne n celice, ki je na pooziciji x, y"""
+    if x >= constants.border_width+constants.square_field_width: return None
+    if y >= constants.border_height+constants.square_field_height: return None
+    if x < constants.border_width: return None
+    if y < constants.border_height: return None
+    x -= constants.border_width
+    y -= constants.border_height
+    sqx = math.floor(x / constants.square_width)
+    sqy = math.floor(y / constants.square_height)
+    return sqx+sqy*constants.number_of_squares[0]
 
 
 def main():
@@ -155,6 +116,7 @@ def main():
                                 graf.sosednjost[sq].remove(kvn)
                             except ValueError:
                                 pass
+                                # ValueError se pojavi, ko poskusimo klikniti na celico, ki je že pravilno označena
 
                 if pygame.mouse.get_pressed()[2]:
                     kvn = kvadrat_na_xy(*pygame.mouse.get_pos())
@@ -165,6 +127,7 @@ def main():
                                 graf.sosednjost[sq].append(kvn)
                             except ValueError:
                                 pass
+                                # ValueError se pojavi, ko poskusimo klikniti na celico, ki je že pravilno označena
 
             if e.type == pygame.MOUSEMOTION:
                 if pygame.mouse.get_pressed()[0]:
@@ -176,6 +139,7 @@ def main():
                                 graf.sosednjost[sq].remove(kvn)
                             except ValueError:
                                 pass
+                                # ValueError se pojavi, ko poskusimo klikniti na celico, ki je že pravilno označena
 
                 if pygame.mouse.get_pressed()[2]:
                     kvn = kvadrat_na_xy(*pygame.mouse.get_pos())
@@ -186,16 +150,17 @@ def main():
                                 graf.sosednjost[sq].append(kvn)
                             except ValueError:
                                 pass
+                                # ValueError se pojavi, ko poskusimo klikniti na celico, ki je že pravilno označena
 
             if e.type == pygame.KEYDOWN:
+
                 if e.key == pygame.K_r:
                     clear_board()
                     in_progress = ""
 
                 if e.key == pygame.K_KP1:
-                    if not bfs_done:
-                        clear_board()
-                        in_progress = "bfs"
+                    clear_board()
+                    in_progress = "bfs"
 
         if in_progress == "bfs":
             if bfs_step(graf, 0, constants.all_squares-1) is True:
